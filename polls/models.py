8361d149bbfa7a6a4fde263d52a286e5d347c52e
gfernandesqmb <guilhermeaugusto@quickmobile.com.br>
2017-08-28 15:40:21 -0300

    Chapter 5

--- polls/models.py
+++ polls/models.py
@@ -12,7 +12,8 @@ class Question(models.Model):
 		return self.question_text
 
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 
@@ -22,5 +23,5 @@ class Choice(models.Model):
 	votes = models.IntegerField(default=0)
 	
 	def __str__(self):
-		return "CHOICE: {}, VOTES: {} for the QUESTION: {}".format(self.choice_text, self.votes, self.question)
+		return self.choice_text
 
