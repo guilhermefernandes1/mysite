53b98a128e3648abfd19149d05f8b5bc6e714af4
Guilherme Anjos <guilhermefernandes1@gmail.com>
2017-08-28 21:58:16 -0300

    Chapter 7

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 
-admin.site.register(Question)
+#class QuestionAdmin(admin.ModelAdmin):
+#	fields = ['pub_date', 'question_text']
+
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3   # How many choices' field by default
+
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('Question',	{'fields': ['question_text'], 'classes': ['colapse']}),
+		('Date Information',	{'fields': ['pub_date'], 'classes': ['colapse']}),
+	]
+	inlines = [ChoiceInline]
+	
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
 
